14059c0792ec65af2e2306d1c530b7ba9abc94f3
Unbit <info@unbit.it>
2014-10-06 21:06:37 +0200

    prepare for rtmpt

--- realtime.h
+++ realtime.h
@@ -10,6 +10,7 @@
 #define REALTIME_UPLOAD 6
 #define REALTIME_INTERLEAVED 7
 #define REALTIME_CHUNKED 8
+#define REALTIME_RTMPT 9
 
 ssize_t urt_redis_pubsub(char *, size_t, int64_t *, char **);
 ssize_t urt_redis_parse(char *, size_t, char *, int64_t *, char **);
